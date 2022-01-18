f7fcdf93d04ee2c8ebacf8823577a15efb898c11
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-01-18 20:46:02 +0100

    fix: front-end's svelte.config.js

--- sources/front-end/svelte.config.js
+++ sources/front-end/svelte.config.js
@@ -1,13 +1,35 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		format: 'esm',
+		hydratable: true,
+	},
+	extensions: ['.svelte'],
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		adapter: adapterNode({
+			precompress: true,
+		}),
+		target: '#schedule',
+		hydrate: true,
+		router: true,
+		vite: {
+			plugins: [{
+				name: 'configure-server',
+				configureServer: (server = null) => {
+					server.middlewares.use((_req, res, next) => {
+						res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+						res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+						next();
+					});
+				},
+			}, ],
+			build: {
+				assetsInlineLimit: 0,
+			},
+		},
+	},
 };
 
 export default config;
