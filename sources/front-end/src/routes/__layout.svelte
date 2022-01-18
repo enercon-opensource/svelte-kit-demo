78d0a26a0db2da3a514719da8921e199a9eae826
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-01-18 21:13:37 +0100

    added styles, icons, layout template, etc

---
+++ sources/front-end/src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import '../app.css';
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    flex: 1 0 auto;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    min-width: 100vw;
+    min-height: 100vh;
+  }
+</style>
