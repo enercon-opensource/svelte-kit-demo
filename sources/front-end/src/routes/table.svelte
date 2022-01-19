9b071c46a936c737e9a49fe0cc725070e31afbac
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-01-19 21:58:22 +0100

    added data table

---
+++ sources/front-end/src/routes/table.svelte
@@ -0,0 +1,40 @@
+<script>
+  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+  import {
+    ContactListArrayStore,
+  } from '$lib/stores/ContactListArrayStore.mjs';
+
+  const contacts = []
+</script>
+
+<style>
+  #data-table-container {
+    width: 50vw;
+    height: 50vh;
+    position: absolute;
+    top: 25%;
+    left: 25%;
+    background-color: hsl(0deg 0% 15%);
+  }
+</style>
+
+<div id='data-table-container'>
+  <DataTable stickyHeader table$aria-label="User list" style="width: 100%;">
+    <Head>
+      <Row>
+        <Cell>email</Cell>
+        <Cell>first name</Cell>
+        <Cell>last name</Cell>
+      </Row>
+    </Head>
+    <Body>
+      {#each $ContactListArrayStore as item (item.email)}
+        <Row>
+          <Cell>{item.email}</Cell>
+          <Cell>{item.firstName}</Cell>
+          <Cell>{item.lastName}</Cell>
+        </Row>
+      {/each}
+    </Body>
+  </DataTable>
+</div>
