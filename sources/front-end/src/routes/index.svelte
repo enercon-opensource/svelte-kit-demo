2305464cba6437734e8fa48a2a96b232d89f8b99
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-01-19 21:18:44 +0100

    added contact list store

--- sources/front-end/src/routes/index.svelte
+++ sources/front-end/src/routes/index.svelte
@@ -1,9 +1,23 @@
 <script>
+  import {
+    onDestroy,
+  } from 'svelte';
+  import {
+    ContactListStore,
+  } from '$lib/stores/ContactListStore.mjs';
   import Form from '$lib/controls/Form.svelte';
+
+  const unsubscribeFromStore = ContactListStore.subscribe((state) => {
+    console.log({ state });
+  });
 
   const handleFormData = ({ detail = null }) => {
     console.log(detail);
+
+    ContactListStore.setContact(detail);
   }
+
+  onDestroy(unsubscribeFromStore);
 </script>
 
 <Form on:formData={handleFormData} />
