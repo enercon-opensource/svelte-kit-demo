520401dc231704ed8c0200e289df6b9086106f26
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-01-19 14:08:15 +0100

    added input form

---
+++ sources/front-end/src/lib/controls/TextInput.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let label;
+  export let name = null;
+
+  let inputId;
+
+  $: if (name !== null) {
+    inputId = `id:${name}`;
+  }
+</script>
+
+<style>
+</style>
+
+{#if inputId !== null}
+  <label for={inputId}>{label}</label>
+  <input type='text' id={inputId} {name} />
+{:else}
+  ...
+{/if}
--- sources/front-end/src/routes/index.svelte
+++ sources/front-end/src/routes/index.svelte
@@ -1,1 +1,75 @@
-input form
+<script>
+  import TextInput from '$lib/controls/TextInput.svelte';
+  import SubmitButton from '$lib/controls/SubmitButton.svelte';
+
+  const textInputsConfigs = [
+    {
+      label: 'first name',
+      name: 'first-name',
+    },
+    {
+      label: 'last name',
+      name: 'last-name',
+    }
+  ];
+
+  const idByName = (name = null) => {
+    if (name === null) {
+      throw new ReferenceError('name is undefined');
+    }
+
+    return `id:${name}`;
+  }
+
+  const handleSumbit = (submitEvent = null) => {
+    console.log(submitEvent);
+  }
+</script>
+
+<style>
+  form {
+    position: absolute;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: var(--grid-gap);
+    align-self: center;
+    top: 50%;
+
+    border: 1px solid white;
+    padding: min(1.5vh, 1.5vw);
+  }
+
+  .form-row,
+  .single-item-row {
+    display: grid;
+    grid-template-rows: 1fr;
+    gap: var(--grid-gap);
+    justify-content: center;
+    align-items: center;
+    padding: min(1.5vh, 1.5vw) min(0.5vh, 0.5vw);
+  }
+  
+  .form-row {
+    grid-template-columns: 1fr 2fr;
+  }
+
+  .single-item-row {
+    grid-template-columns: 1fr;
+  }
+</style>
+
+<form on:submit|preventDefault|stopPropagation={handleSumbit}>
+  {#each textInputsConfigs as textInputsConfig}
+    {@const textInputId = idByName(textInputsConfig.name)}
+
+    <div class='form-row'>
+      <label for={textInputId}>{textInputsConfig.label}</label>
+      <input type='text' id={textInputId} name={textInputsConfig.name} />
+    </div>
+  {/each}
+
+  <div class='single-item-row'>
+    <SubmitButton />
+  </div>
+</form>
